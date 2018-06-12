e2787aeea575c5124c39a33328c1b0f99fa0e119
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-06-12 01:34:14 -0500

    close btn on touch

--- scripts.js
+++ scripts.js
@@ -6,15 +6,12 @@ window.onload = function(){
     navTopWrap.classList.toggle("open");
   }
 
-  window.onclick = function(event){
+  window.onclick = close_btn;
+  window.addEventListener('touchstart', close_btn);
+
+  function close_btn(event){
     if(!event.target.matches('#navbar-toggler')){
       navTopWrap.classList.remove('open');   
     }
   }
-
-  window.addEventListener('touchstart', function(ev) {
-    if(!ev.target.matches('#navbar-toggler')){
-      navTopWrap.classList.remove('open');   
-    }
-  });
 }
