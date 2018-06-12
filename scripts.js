c406da72ef574316e950f3fb0b45fafb2849a879
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-06-12 00:24:35 -0500

    toggle btn JS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 window.onload = function(){
+  var button = document.getElementById("navbar-toggler");
+  var navTopWrap = document.getElementById("navTopWrap");
 
+  button.onclick = function(){
+    navTopWrap.classList.toggle("open");
+  }
+
+  window.onclick = function(event){
+    if(!event.target.matches('#navbar-toggler')){
+      navTopWrap.classList.remove('open');   
+    }
+  }
 }
