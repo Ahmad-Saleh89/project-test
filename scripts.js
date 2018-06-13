bd6dceea92bc9eb1ee64e5baa42f19bac7969e31
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2018-06-13 00:16:24 -0500

    Some Enhancement

--- scripts.js
+++ scripts.js
@@ -2,10 +2,12 @@ window.onload = function(){
   var button = document.getElementById("navbar-toggler");
   var navTopWrap = document.getElementById("navTopWrap");
 
+  // Toggle .open class when Hamburger button is clicked
   button.onclick = function(){
     navTopWrap.classList.toggle("open");
   }
 
+  // Remove .open class when click out
   window.onclick = close_btn;
   window.addEventListener('touchstart', close_btn);
 
